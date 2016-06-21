7d1879b871bfad87b4e469543953084027494cdf
John Brilla <john.brilla@thesearchparty.com>
2016-06-21 17:12:58 +1000

    init commit

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,66 @@
+defmodule Todo.UserController do
+	use Todo.Web, :controller
+	alias Todo.User
+
+	def index(conn, _params) do
+		users = Repo.all(User)
+		render(conn, "index.html", users: users)
+	end
+
+	def new(conn, _params) do
+		changeset = User.changeset(%User{})
+		render(conn, "new.html", changeset: changeset)
+	end
+
+	def create(conn, %{"user" => user_params}) do
+		changeset = User.registration_changeset(%User{}, user_params)
+		case Repo.insert(changeset) do
+			{:ok, user} ->
+				conn
+				|> Todo.Auth.login(user)
+				|> put_flash(:info, "User create!")
+				|> redirect(to: user_path(conn, :index))
+			{:error, changeset} ->
+				conn
+				|> render("new.html", changeset: changeset)
+		end
+	end
+
+	def show(conn, %{"id" => id}) do
+	    user = Repo.get(User, id)
+	    changeset = User.changeset(user)
+	    cond do
+	      user == Guardian.Plug.current_resource(conn) ->
+	        conn
+	        |> render("show.html", user: user, changeset: changeset)
+	      :error ->
+	        conn
+	        |> put_flash(:info, "No access")
+	        |> redirect(to: page_path(conn, :index))
+	    end
+	end
+
+	def update(conn, %{"id" => id, "user" => user_params}) do
+		user = Repo.get(User, id)
+		changeset = User.registration_changeset(user, user_params)
+
+		cond do
+			user == Guardian.Plug.current_resource(conn) ->
+				case Repo.update(changeset) do
+					{:ok, user} ->
+						conn
+						|> put_flash(:info, "User update")
+						|> redirect(to: page_path(conn, :index))
+					{:error, changeset} ->
+						conn
+						|> render("show.html", user: user, changeset: changeset)
+				end
+				:error ->
+					conn
+					|> put_flash(:info, "No Access")
+					|> redirect(to: page_path(conn, :index))
+
+		end		
+	end
+
+end
